9ad80559c7078ac8efc53d90fc51f8b67038342c
Oliver Wegner <void1976@gmail.com>
2013-11-28 00:46:34 +0100

    added myadd function

--- ctest/ctest.c
+++ ctest/ctest.c
@@ -5,13 +5,26 @@ int main(int argc, char* argv[]) {
 	LLVMModuleRef module = LLVMModuleCreateWithNameInContext("mytop", context);
 	LLVMBuilderRef builder = LLVMCreateBuilderInContext(context);
 
-	// declare puts:
+	// get some types:
 	LLVMTypeRef i8Type = LLVMInt8TypeInContext(context);
 	LLVMTypeRef i8PtrType = LLVMPointerType(i8Type, 0);
-	LLVMTypeRef putsParams[] = { i8PtrType };
 	LLVMTypeRef i32Type = LLVMInt32TypeInContext(context);
+
+	// declare puts:
+	LLVMTypeRef putsParams[] = { i8PtrType };
 	LLVMTypeRef putsFuncType = LLVMFunctionType(i32Type, putsParams, 1, 0);
 	LLVMValueRef putsFunc = LLVMAddFunction(module, "puts", putsFuncType);
+
+	// define myadd:
+	LLVMTypeRef addParams[] = { i32Type, i32Type };
+	LLVMTypeRef addFuncType = LLVMFunctionType(i32Type, addParams, 2, 0);
+	LLVMValueRef addFunc = LLVMAddFunction(module, "myadd", addFuncType);
+	LLVMBasicBlockRef addEntry = LLVMAppendBasicBlockInContext(context, addFunc, "myentry");
+	LLVMPositionBuilderAtEnd(builder, addEntry);
+	LLVMValueRef addParam0 = LLVMGetParam(addFunc, 0);
+	LLVMValueRef addParam1 = LLVMGetParam(addFunc, 1);
+	LLVMValueRef sum = LLVMBuildAdd(builder, addParam0, addParam1, "");
+	LLVMBuildRet(builder, sum);
 
 	// define main:
 	LLVMTypeRef mainFuncType = LLVMFunctionType(i32Type, 0, 0, 0);
@@ -19,12 +32,21 @@ int main(int argc, char* argv[]) {
 
 	LLVMBasicBlockRef mainEntry = LLVMAppendBasicBlockInContext(context, mainFunc, "myentry");
 	LLVMPositionBuilderAtEnd(builder, mainEntry);
+
+	// define global vars:
 	LLVMValueRef helloWorld = LLVMBuildGlobalStringPtr(builder, "hello world in c!\n", "");
 
+	// call puts:
 	LLVMValueRef putsArgs[] = { helloWorld };
-	LLVMValueRef putsCall = LLVMBuildCall(builder, putsFunc, putsArgs, 1, "");
+	LLVMValueRef putsResult = LLVMBuildCall(builder, putsFunc, putsArgs, 1, "");
 
-	LLVMValueRef retVoid = LLVMBuildRetVoid(builder);
+	// call myadd:
+	LLVMValueRef addArg0 = LLVMConstInt(i32Type, 34, 0);
+	LLVMValueRef addArg1 = LLVMConstInt(i32Type, 11, 0);
+	LLVMValueRef addArgs[] = { addArg0, addArg1 };
+	LLVMValueRef addResult = LLVMBuildCall(builder, addFunc, addArgs, 2, "");
+
+	LLVMValueRef retVoid = LLVMBuildRet(builder, addResult);
 
 	LLVMDumpModule(module);
 
